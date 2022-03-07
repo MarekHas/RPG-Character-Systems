2dbb0cfc0c0cd6affcd31b438c938d21f285e1cd
MarekHas <marek.hasulak@gmail.com>
2022-03-07 19:37:42 +0100

    add node runtime

---
+++ Assets/Common/Scripts/Runtime/Nodes/AddNode.cs
@@ -0,0 +1,55 @@
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using UnityEngine;
+
+namespace Common.Nodes
+{
+    public class AddNode : IntermediateNode
+    {
+        [HideInInspector] public FunctionNode addendA;
+        [HideInInspector] public FunctionNode addendB;
+        public override float Value => addendA.Value + addendB.Value;
+        public override void RemoveChild(FunctionNode child, string portName)
+        {
+            if (portName.Equals("A"))
+            {
+                addendA = null;
+            }
+            else
+            {
+                addendB = null;
+            }
+        }
+
+        public override void AddChild(FunctionNode child, string portName)
+        {
+            if (portName.Equals("A"))
+            {
+                addendA = child;
+            }
+            else
+            {
+                addendB = child;
+            }
+        }
+
+        public override ReadOnlyCollection<FunctionNode> children
+        {
+            get
+            {
+                List<FunctionNode> nodes = new List<FunctionNode>();
+                if (addendA != null)
+                {
+                    nodes.Add(addendA);
+                }
+
+                if (addendB != null)
+                {
+                    nodes.Add(addendB);
+                }
+
+                return nodes.AsReadOnly();
+            }
+        }
+    }
+}
